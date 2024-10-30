4d236cc2216b923a3786ad60a1b40ea5b8318fae
Vale088 <valerio.sinni@marconibari.edu.it>
2024-10-30 18:59:19 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,43 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int i,negativi,p;
+    float min,numero,somma,max,media;
+    somma=0;
+    i=1;
+    negativi=0;
+    p=0;
+     do { 
+        cout<<"inserisci un numero"<<endl;
+        cin>>numero;
+         if ( numero>=0) {
+            somma=somma+numero; 
+            p=p+1;
+         } 
+        if (numero<0) {
+            negativi=negativi+1;
+        } 
+        if (i==1) {
+            max=numero;
+            min=numero;
+         }else{ 
+            if(numero>max) {
+                max=numero;
+            } 
+            if (numero<min) {
+                min=numero;
+            }
+
+        }  
+        i=i+1;
+         }while (numero !=0);
+         media=somma/p;
+          cout<<"massimo: "<< max <<endl;
+          cout<<"minimo: " << min <<endl;
+          cout<<"media numeri positivi:"<< media<<endl;
+          cout<<"numeri negativi: "<<negativi<<endl;
+           }
+     }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
